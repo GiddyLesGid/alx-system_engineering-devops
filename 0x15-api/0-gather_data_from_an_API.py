e93242fa2ae3984bdd9f3b9ef9736b84d1f290e4
GiddyLesGid <gideonlangat@gmail.com>
2023-05-22 10:33:07 -0700

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,13 +4,17 @@ import requests
 import sys
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Please provide the employee ID as a command-line argument.")
+        sys.exit(1)
+
+    employee_id = sys.argv[1]
+
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    user = requests.get(url + "users/{}".format(employee_id)).json()
+    todos = requests.get(url + "todos", params={"userId": employee_id}).json()
 
     completed = [t.get("title") for t in todos if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
-
-    for c in completed:
-        print("\t{}".format(c))
+    [print("\t {}".format(c)) for c in completed]
