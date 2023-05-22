bc16bc64b6b4adc6a943222ae15633f598243931
GiddyLesGid <gideonlangat@gmail.com>
2023-05-22 03:59:13 -0700

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,28 +1,37 @@
 #!/usr/bin/python3
 
+"""
+Returns TODO list progress for a given employee ID and saves it to a file.
+"""
+
 import sys
-from urllib import request
+import requests
 import json
 
 if __name__ == "__main__":
-    base_url = "https://jsonplaceholder.typicode.com/"
+    if len(sys.argv) < 2:
+        sys.exit("Usage: python script.py <employee_id>")
+
     employee_id = sys.argv[1]
-    
+    base_url = "https://jsonplaceholder.typicode.com/"
+
     # Retrieve user information
-    user_url = base_url + "users/{}".format(employee_id)
-    user_response = request.urlopen(user_url)
-    user_data = json.loads(user_response.read().decode())
+    user_response = requests.get(base_url + "users/{}".format(employee_id))
+    user_data = user_response.json()
 
     # Retrieve TODO list information
-    todos_url = base_url + "todos?userId={}".format(employee_id)
-    todos_response = request.urlopen(todos_url)
-    todos_data = json.loads(todos_response.read().decode())
-    
-    completed = [t.get("title") for t in todos_data if t.get("completed") is True]
+    todos_response = requests.get(base_url + "todos", params={"userId": employee_id})
+    todos_data = todos_response.json()
+
+    completed = [task.get("title") for task in todos_data if task.get("completed")]
     total_tasks = len(todos_data)
-    
-    print("Employee {} is done with tasks({}/{}):".format(
-        user_data.get("name"), len(completed), total_tasks))
-    
+
+    print("Employee {} is done with tasks({}/{}):".format(user_data.get("name"), len(completed), total_tasks))
     for task_title in completed:
         print("\t{}".format(task_title))
+    
+    # Save TODO list to a file
+    filename = "todos_employee_{}.json".format(employee_id)
+    with open(filename, "w") as file:
+        json.dump(todos_data, file)
+    print("TODO list saved to", filename)
